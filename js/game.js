c6571caea62c9cac433b9941d0dd5f8249edc1da
qparker0204 <quintenparker@comcast.net>
2017-10-04 19:07:15 -0700

    added player movement

--- js/game.js
+++ js/game.js
@@ -25,6 +25,7 @@ Game.create = function() {
   //Allows clicking on the map
   layer.inputEnabled = true;
   Client.askNewPlayer();
+  layer.events.onInputUp.add(Game.getCoordinates, this);
 };
 
 //add player to map
@@ -35,4 +36,17 @@ Game.addNewPlayer = function(id,x,y) {
 Game.removePlayer = function(id) {
   Game.playerMap[id].destroy();
   delete Game.playerMap[id];
-}
+};
+
+Game.getCoordinates = function(layer, pointer) {
+  Client.sendClick(pointer.worldX, pointer.worldY);
+};
+
+Game.movePlayer = function(id, x, y) {
+  var player = Game.playerMap[id];
+  var distance = Phaser.Math.distance(player.x, player.y, x, y);
+  var duration = distance * 10;
+  var tween = game.add.tween(player);
+  tween.to({x:x, y:y}, duration);
+  tween.start();
+};
